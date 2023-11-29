57f7e4c7d6ce2e2502b9a438d831efc71bcb1109
Carter Conboy <group64@elec377.queensu.ca>
2023-11-28 20:01:45 -0500

    Test code

--- train.py
+++ train.py
@@ -4,18 +4,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim import lr_scheduler
 import torch.backends.cudnn as cudnn
 import numpy as np
-import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
-import time
-import os
-from PIL import Image
-from tempfile import TemporaryDirectory
 from dataset import DoggyDataset
-import cv2
 import argparse
 
 data_transform = transforms.Compose([
@@ -41,7 +34,6 @@ parser.add_argument('-e', type=int, default = 1)
 args = parser.parse_args()
 Epochs = args.e
 Batches = args.b
-Cuda = args.cuda
 Weights = args.weights
 Dataset_Path = args.dataset
 Label_Path = args.label
@@ -56,20 +48,16 @@ train_dataset = DoggyDataset(label_file, data_dir, transform=data_transform)
 train_dataloader = torch.utils.data.DataLoader(train_dataset, Batches, shuffle=True)
 
 #Load Model
-if(Weights):
-        model_ft = models.resnet18()
-        model_ft.load_state_dict(torch.load(Weights, map_location=torch.device(device)))
-else:
-        model_ft = models.resnet18()
-
+model_ft = models.resnet18()
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, 2)
+if(Weights):
+        model_ft.load_state_dict(torch.load(Weights, map_location=torch.device(device)))
 model_ft = model_ft.to(device)
 
 #Load Optimizer, Loss function
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model_ft.parameters(), lr=0.0001)
-
 
 #Run train for Epoch
 loss_at_epoch = torch.zeros(Epochs)
